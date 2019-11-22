1747cb8a366eda52158264c0a461cc29a7fd92da
Alex Carpenedo <alex_carpenedo@hotmail.com>
2019-11-22 12:59:24 -0300

    commit

---
+++ medicina/src/main/java/fadep/medicina/MedicinaApplication.java
@@ -0,0 +1,13 @@
+package fadep.medicina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicinaApplication.class, args);
+	}
+
+}
---
+++ medicina/src/test/java/fadep/medicina/MedicinaApplicationTests.java
@@ -0,0 +1,13 @@
+package fadep.medicina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
