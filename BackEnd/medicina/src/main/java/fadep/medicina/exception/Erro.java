f28d24b46672aef24ba677910c7a3cf318b86567
leonardovere <leonardovere@hotmail.com>
2019-12-10 15:13:07 -0300

    Pacote exception criado, contendo as classes de Exception e Erro

---
+++ BackEnd/medicina/src/main/java/fadep/medicina/exception/Erro.java
@@ -0,0 +1,22 @@
+package fadep.medicina.exception;
+
+public class Erro {
+
+	private String mensagemUsuario;
+	private String mensagemDesenvolvedor;
+	
+	public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+		super();
+		this.mensagemUsuario = mensagemUsuario;
+		this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+	}
+
+	public String getMensagemUsuario() {
+		return mensagemUsuario;
+	}
+
+	public String getMensagemDesenvolvedor() {
+		return mensagemDesenvolvedor;
+	}
+	
+}
