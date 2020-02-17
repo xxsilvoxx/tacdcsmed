547a5288182441c4a8a51f94e4135ce0aef01c2c
xxsilvoxx <37382434+xxsilvoxx@users.noreply.github.com>
2020-02-17 14:00:20 -0300

    comit de import, resolvendo conflito de componente
    resolvivo o conflito de componente visitas, corrigo, se der problema no de vocês basta  ignorar o ultimo push que eu comitei hoje dia 17-02-2020, ai aceita as novas alterações no visual studio.

--- FrontEnd/mindsafe/src/app/visitas/visitas.module.ts
+++ FrontEnd/mindsafe/src/app/visitas/visitas.module.ts
@@ -5,12 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import { VisitasRoutingModule } from './visitas-routing.module';
-<<<<<<< HEAD
-import { VisitasComponent } from './visitas/visitas.component';
-=======
 import { VisitasComponent } from './visitas.component';
 import {MatListModule} from '@angular/material/list';
->>>>>>> efed9684837c3cbf8094f6d8277d6026dc8bd742
 
 
 @NgModule({
