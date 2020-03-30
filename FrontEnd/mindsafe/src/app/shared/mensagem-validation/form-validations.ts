ceaec31d59c01f708882bbc06685c96978c59893
Your Name <leonardovere@hotmail.com>
2020-03-29 21:02:01 -0300

    Alterando validação de cpf e cnpj no front

--- FrontEnd/mindsafe/src/app/shared/mensagem-validation/form-validations.ts
+++ FrontEnd/mindsafe/src/app/shared/mensagem-validation/form-validations.ts
@@ -1,7 +1,9 @@
-import { FuncionariosService } from '../../services/funcionarios/funcionarios.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { timer } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
+
+import { FuncionariosService } from '../../services/funcionarios/funcionarios.service';
+import { PacientesService } from '../../services/pacientes/pacientes.service';
 
 // Validação pra mostrar se o login está disponivel
 // Percore no banco pra todos os funcionários
@@ -22,6 +24,19 @@ export const emailDisponivelValidator = (service: FuncionariosService, time: num
       switchMap(() => service.verificarEmail(input.value)),
       map(res => {
         return res ? null : { emailInvalido: true };
+      })
+    );
+  };
+};
+
+export const cpfCnpjDisponivelValidator = (service: PacientesService, time: number = 500) => {
+  return (input: FormControl) => {
+    return timer (time).pipe(
+      switchMap(() => (input.value.length === 14 || input.value.length === 18)
+      ? service.retornarCpfCnpjValido(input.value)
+      : null),
+      map(res => {
+        return res ? null : { cpfCnpjInvalido: true };
       })
     );
   };
