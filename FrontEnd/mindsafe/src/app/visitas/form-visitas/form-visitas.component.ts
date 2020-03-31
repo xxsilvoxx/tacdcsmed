a098112bafd787a3cd97c4723bc36d2bf401e913
xxsilvoxx <37382434+xxsilvoxx@users.noreply.github.com>
2020-03-31 16:39:14 -0300

    Teste de Comit
    Criado a Modal para inserir o HTML, conform e o mdoel

---
+++ FrontEnd/mindsafe/src/app/visitas/form-visitas/form-visitas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-visitas',
+  templateUrl: './form-visitas.component.html',
+  styleUrls: ['./form-visitas.component.scss']
+})
+export class FormVisitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- FrontEnd/mindsafe/src/app/visitas/visitas.module.ts
+++ FrontEnd/mindsafe/src/app/visitas/visitas.module.ts
@@ -1,3 +1,4 @@
+import {  MatDialogModule } from '@angular/material/dialog';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
@@ -8,10 +9,12 @@ import { MatListModule } from '@angular/material/list';
 import { VisitasRoutingModule } from './visitas-routing.module';
 import { VisitasComponent } from './visitas/visitas.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormVisitasComponent } from './form-visitas/form-visitas.component';
 
 @NgModule({
   declarations: [
-    VisitasComponent
+    VisitasComponent,
+    FormVisitasComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +23,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MatDialogModule,
     FlexLayoutModule
+  ],
+  entryComponents: [
+    FormVisitasComponent
   ]
 })
 export class VisitasModule { }
