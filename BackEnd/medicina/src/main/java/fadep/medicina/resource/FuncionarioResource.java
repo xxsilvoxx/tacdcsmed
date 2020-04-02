eb77f8ddfc670db7191f71340a0427e417e91200
Leonardo Roberto Candido <leonardovere@hotmail.com>
2020-04-02 15:15:16 -0300

    Alteração no backend, movendo coluna ativo pra usuário e os metodos mudados para o funcionarioResource

--- BackEnd/medicina/src/main/java/fadep/medicina/resource/FuncionarioResource.java
+++ BackEnd/medicina/src/main/java/fadep/medicina/resource/FuncionarioResource.java
@@ -3,6 +3,7 @@ package fadep.medicina.resource;
 import fadep.medicina.model.Funcionario;
 import fadep.medicina.repository.FuncionarioRepository;
 import fadep.medicina.service.FuncionarioService;
+import fadep.medicina.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ public class FuncionarioResource {
 
     @Autowired
     public FuncionarioService funcionarioService;
+
+    @Autowired
+    public UsuarioService usuarioService;
 
     @GetMapping
     public List<Funcionario> listarTodos() {
@@ -65,4 +69,9 @@ public class FuncionarioResource {
         return funcionarioService.remover(codigo);
     }
 
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+        usuarioService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
 }
--- BackEnd/medicina/src/main/java/fadep/medicina/service/FuncionarioService.java
+++ BackEnd/medicina/src/main/java/fadep/medicina/service/FuncionarioService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class FuncionarioService {
 
