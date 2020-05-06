3622cfedb9aa7772f38179e76216f8dbbe7c3063
Leonardo Roberto Candido <leonardovere@hotmail.com>
2020-05-05 23:59:32 -0300

    Finalizando component de página não encontrada, adicionando animações css e licensa da imagem

--- FrontEnd/mindsafe/src/app/app-routing.module.ts
+++ FrontEnd/mindsafe/src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'mindsafe', loadChildren: () => import('./menu-sidenav/menu-sidenav.module').then(m => m.MenuSidenavModule) },
   {
     path: '404',
     loadChildren: () => import('./pagina-nao-encontrada/pagina-nao-encontrada.module').then(m => m.PaginaNaoEncontradaModule)
   },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'mindsafe', loadChildren: () => import('./menu-sidenav/menu-sidenav.module').then(m => m.MenuSidenavModule) },
   { path: '**', redirectTo: '404' },
 ];
 
